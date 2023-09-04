dbf68ee78f0b7e8d2dfb011415ca196c5c3c8dc1
logesh-1310 <logesh-1310@logesh-1310.csez.zohocorpin.com>
2023-09-04 14:57:36 +0530

    Elements

---
+++ Object Repository/My Page/ele.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ele</name>
+   <tag></tag>
+   <elementGuidId>677aa9c5-c284-4e33-adfe-45cb9a10a051</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/New Test Object.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>New Test Object</name>
+   <tag></tag>
+   <elementGuidId>67027bd8-3a6f-4d7f-974a-125a7759a724</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
